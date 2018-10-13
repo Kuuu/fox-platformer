e130ab262516180eeac913fb4053095888b44c0f
Ilya Kudinov <i.kudinov2@gmail.com>
2018-10-13 17:16:37 +0300

    Added the unity project

---
+++ PlatformerSandbox/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothTime = 0.15f;
+
+    public float offsetY = -10f;
+    public float crouchOffsetY = -30f;
+    private float currentOffset;
+
+    private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+        currentOffset = offsetY;
+        GameObject.Find("Player").GetComponent<CharacterController2D>().OnCrouchEvent.AddListener(OnCrouch);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(target.position.x, target.position.y + currentOffset, transform.position.z), ref velocity, smoothTime);	
+	}
+
+    void OnCrouch(bool isCrouching)
+    {
+        if (isCrouching)
+        {
+            currentOffset = crouchOffsetY;
+        } else
+        {
+            currentOffset = offsetY;
+        }
+    }
+}
---
+++ PlatformerSandbox/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    CharacterController2D character;
+    Animator animator;
+
+    public float speed = 40f;
+    private float movement = 0f;
+    private bool jump = false;
+    private bool crouch;
+
+    public float bottom = -10f;
+
+
+	// Use this for initialization
+	void Start () {
+        character = GetComponent<CharacterController2D>();
+        animator = GetComponent<Animator>();
+        GetComponent<CharacterController2D>().OnCrouchEvent.AddListener(OnCrouchEvent);
+	}
+
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetTrigger("jump");
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+        if (movement != 0)
+        {
+            animator.SetBool("running", true);
+        } else
+        {
+            animator.SetBool("running", false);
+        }
+
+        if (transform.position.y <= bottom)
+        {
+            GameController.Instance.RestartLevel();
+        }
+    }
+	
+	void FixedUpdate () {
+        character.Move(movement * speed * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+	}
+
+    void OnCrouchEvent(bool isCrouching)
+    {
+        if (isCrouching)
+        {
+            animator.SetBool("crouch", true);
+            animator.SetTrigger("startCrouch");
+        } else
+        {
+            animator.SetBool("crouch", false);
+        }
+    }
+}
